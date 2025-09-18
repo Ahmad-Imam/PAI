8ac6afebf8b25f7f4080e0ed3e7735ddcfda3db5
Ahmad-Imam <ahmadimam71@gmail.com>
2025-09-18 11:10:14 +0300

    unused cleanup

--- src/app/convex-client-provider.tsx
+++ src/app/convex-client-provider.tsx
@@ -1,21 +1,19 @@
-"use client"
+"use client";
 
-import { ConvexClient } from "convex/browser"
-import {ConvexReactClient} from "convex/react"
-import {ConvexProvider} from "convex/react"
-import {useState} from "react"
-import {ConvexAuthNextjsProvider} from "@convex-dev/auth/nextjs"
+import { ConvexReactClient } from "convex/react";
 
+import { ConvexAuthNextjsProvider } from "@convex-dev/auth/nextjs";
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!)
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export function ConvexClientProvider({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-
-return <ConvexAuthNextjsProvider client={convex}>{children}</ConvexAuthNextjsProvider>
-
-
+  return (
+    <ConvexAuthNextjsProvider client={convex}>
+      {children}
+    </ConvexAuthNextjsProvider>
+  );
 }
